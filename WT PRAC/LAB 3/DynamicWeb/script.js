ca93464bb8528bf282bca613db3a8d0acd41548a
angreatharva <angreatharva08@gmail.com>
2024-10-08 09:54:39 +0530

    wt lb 3

--- WT PRAC/LAB 3/DynamicWeb/script.js
+++ WT PRAC/LAB 3/DynamicWeb/script.js
@@ -1,9 +1,20 @@
 function logShoeDetails(element) {
   const name = element.getAttribute("data-name");
+  const image = element.getAttribute("data-image");
   const description = element.getAttribute("data-description");
   const price = element.getAttribute("data-price");
 
   console.log(`Name: ${name}`);
+  console.log(`Image: ${image}`);
   console.log(`Description: ${description}`);
   console.log(`Price: ${price}`);
+
+  // Store the product details in localStorage or sessionStorage
+  localStorage.setItem("productName", name);
+  localStorage.setItem("productImage", image);
+  localStorage.setItem("productDescription", description);
+  localStorage.setItem("productPrice", price);
+
+  // Navigate to the product details page
+  window.location.href = "productsDetail.html";
 }
