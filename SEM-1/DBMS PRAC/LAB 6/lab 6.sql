28a5bf12cb48fc4bb3c3af03b81d4fbabb8ca96d
angreatharva <angreatharva08@gmail.com>
2025-01-17 12:53:11 +0530

    commit

--- SEM-1/DBMS PRAC/LAB 6/lab 6.sql
+++ SEM-1/DBMS PRAC/LAB 6/lab 6.sql
@@ -27,4 +27,23 @@ call exceptionHandling4(2,003);
 call exceptionHandling4(2,003);
 call exceptionHandling4(2,004);
 
+drop table t1;
+drop table t2;
+drop table t3;
+
+create table t3 (t1id1 int PRIMARY KEY,t2id2 int UNIQUE);
+create table t1 (
+id1 int, 
+name varchar(255),
+place varchar(255), 
+constraint fk_t1 foreign key (id1) references t3(t1id1)
+);
+
+create table t2 (
+id2 int,
+visited varchar(255),
+cost int,
+CONSTRAINT fk_t2 FOREIGN KEY (id2) REFERENCES t3(t2id2)
+);
+
 
