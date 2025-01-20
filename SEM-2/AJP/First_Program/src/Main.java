aaef58f9d9650db80458496d36e8d06245491b2a
angreatharva <angreatharva08@gmail.com>
2025-01-20 15:28:45 +0530

    AJP prac session 1

---
+++ SEM-2/AJP/First_Program/src/Main.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+
+class BiggestOfThree {
+    public void findBiggest(int num1, int num2, int num3) {
+        if (num1 > num2 && num1 > num3)
+            System.out.println("Num 1 is greater");
+        else if (num2 > num1 && num2 > num3)
+            System.out.println("Num 2 is greater");
+        else
+            System.out.println("Num 3 is greater");
+    }
+}
+
+class Result {
+    public void calculateResult(Scanner sc) {
+        System.out.println("\nEnter the Marks of the subject");
+
+        System.out.println("Enter the Marks of DAA subject: ");
+        int DAA = sc.nextInt();
+
+        System.out.println("Enter the Marks of DevOps subject: ");
+        int DevOps = sc.nextInt();
+
+        System.out.println("Enter the Marks of AJP subject: ");
+        int AJP = sc.nextInt();
+
+        System.out.println("Enter the Marks of MP subject: ");
+        int MP = sc.nextInt();
+
+        System.out.println("Enter the Marks of PA subject: ");
+        int PA = sc.nextInt();
+
+        System.out.println("Enter the Marks of PCS subject: ");
+        int PCS = sc.nextInt();
+
+        System.out.println("Enter the Marks of PNS subject: ");
+        int PNS = sc.nextInt();
+
+        int avg = (DAA + DevOps + AJP + MP + PA + PCS + PNS) / 7;
+
+        System.out.println("Percentage of the stundent is: " + avg);
+
+        if (avg >= 90)
+            System.out.println("The Student got Grade A+");
+        else if (avg >= 80)
+            System.out.println("The Student got Grade A");
+        else if (avg >= 70)
+            System.out.println("The Student got Grade B+");
+        else if (avg >= 60)
+            System.out.println("The Student got Grade B");
+        else if (avg >= 50)
+            System.out.println("The Student got Grade C");
+        else if (avg >= 35)
+            System.out.println("The Student got Grade D");
+        else
+            System.out.println("The Student got Grade F");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter value of num 1: ");
+        // int num1 = sc.nextInt();
+        // System.out.println("Enter value of num 2: ");
+        // int num2 = sc.nextInt();
+        // System.out.println("Enter value of num 3: ");
+        // int num3 = sc.nextInt();
+
+        // BiggestOfThree biggest = new BiggestOfThree();
+        // biggest.findBiggest(num1, num2, num3);
+        
+        // Result result = new Result();
+        // result.calculateResult(sc);
+
+        
+
+
+        hourlyBasis hourlyBasisEmployee = new hourlyBasis();
+        hourlyBasisEmployee.getData(1, "Atharva");
+        hourlyBasisEmployee.getHourlyData(1, 800);
+        hourlyBasisEmployee.calculate();
+        System.out.println("Hourly Basis Employee Details:");
+        hourlyBasisEmployee.putData();
+
+        monthlyBasis monthlyBasisEmployee = new monthlyBasis();
+        monthlyBasisEmployee.getData(2, "Adam");
+        monthlyBasisEmployee.getMonthlyData(15000, 10,1 );
+        monthlyBasisEmployee.calculate();
+        System.out.println("\nMonthly Basis Employee Details:");
+        monthlyBasisEmployee.putData();
+    }
+}
---
+++ SEM-2/AJP/First_Program/src/employee.java
@@ -0,0 +1,47 @@
+class employee {
+    private int employeeNumber;
+    private String employeeName;
+    protected int salary;
+
+    public void getData(int empno, String empname) {
+        employeeNumber = empno;
+        employeeName = empname;
+    }
+    
+    public void putData() {
+        System.out.println("Employee Number: " + employeeNumber);
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Employee Salary: " + salary);
+    }
+}
+
+class hourlyBasis extends employee {
+    private int hours;
+    private int rate;
+
+    public void getHourlyData(int h, int r) {
+        hours = h;
+        rate = r;
+    }
+
+    public void calculate() {
+        salary = hours * rate;
+    }
+}
+
+class monthlyBasis extends employee {
+    private int basic;
+    private int hra;
+    private int da;
+
+    public void getMonthlyData(int b, int h, int d) {
+        basic = b;
+        hra = h;
+        da = d;
+    }
+
+    public void calculate() {
+        salary = basic + (basic * hra / 100) + (basic * da / 100);
+    }
+    
+}
