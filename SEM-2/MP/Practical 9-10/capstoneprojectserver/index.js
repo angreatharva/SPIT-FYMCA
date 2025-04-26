df4f5d13505eb4f9da4a5dc39829ada12ac5af25
angreatharva <angreatharva08@gmail.com>
2025-04-26 18:18:01 +0530

    commit 9-10 mp

--- SEM-2/MP/Practical 9-10/capstoneprojectserver/index.js
+++ SEM-2/MP/Practical 9-10/capstoneprojectserver/index.js
@@ -31,7 +31,6 @@ app.use('/api/transcriptions', require('./routes/transcription.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/doctors', require('./routes/doctor.routes'));
 app.use('/api/health', require('./routes/health.routes'));
-app.use('/api/call-requests', require('./routes/call-request.routes'));
 
 // Create HTTP server and attach socket.io
 const server = app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
